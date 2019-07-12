b28262ec31dbc1b7bd42220482e176ade833bf6c
radoikoff <radoikoff@gmail.com>
2019-07-12 16:58:02 +0300

    Login and Register pages works now with custom user. Initial DB creation.

--- SkyHigh.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SkyHigh.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -17,12 +17,12 @@ namespace SkyHigh.Web.Areas.Identity.Pages.Account
     public class LoginModel : PageModel
     {
         private readonly SignInManager<SkyHighUser> _signInManager;
-        private readonly ILogger<LoginModel> _logger;
+        //private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<SkyHighUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<SkyHighUser> signInManager)//, ILogger<LoginModel> logger)
         {
             _signInManager = signInManager;
-            _logger = logger;
+            //_logger = logger;
         }
 
         [BindProperty]
@@ -38,8 +38,7 @@ namespace SkyHigh.Web.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            public string Username { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
@@ -74,21 +73,21 @@ namespace SkyHigh.Web.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+                var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User logged in.");
+                   // _logger.LogInformation("User logged in.");
                     return LocalRedirect(returnUrl);
                 }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out.");
-                    return RedirectToPage("./Lockout");
-                }
+                //if (result.RequiresTwoFactor)
+                //{
+                //    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
+                //}
+                //if (result.IsLockedOut)
+                //{
+                //    //_logger.LogWarning("User account locked out.");
+                //    return RedirectToPage("./Lockout");
+                //}
                 else
                 {
                     ModelState.AddModelError(string.Empty, "Invalid login attempt.");
--- SkyHigh.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SkyHigh.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -18,19 +18,19 @@ namespace SkyHigh.Web.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<SkyHighUser> _signInManager;
         private readonly UserManager<SkyHighUser> _userManager;
-        private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
+        //private readonly ILogger<RegisterModel> _logger;
+        //private readonly IEmailSender _emailSender;
 
         public RegisterModel(
             UserManager<SkyHighUser> userManager,
-            SignInManager<SkyHighUser> signInManager,
-            ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            SignInManager<SkyHighUser> signInManager)
+        //  ILogger<RegisterModel> logger,
+        //  IEmailSender emailSender)
         {
             _userManager = userManager;
             _signInManager = signInManager;
-            _logger = logger;
-            _emailSender = emailSender;
+            // _logger = logger;
+            // _emailSender = emailSender;
         }
 
         [BindProperty]
@@ -41,12 +41,16 @@ namespace SkyHigh.Web.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [Display(Name = "User Name")]
+            public string Username { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
@@ -55,6 +59,10 @@ namespace SkyHigh.Web.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            [Display(Name = "Full Name")]
+            public string Fullname { get; set; }
         }
 
         public void OnGet(string returnUrl = null)
@@ -67,21 +75,28 @@ namespace SkyHigh.Web.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new SkyHighUser { UserName = Input.Email, Email = Input.Email };
+                var user = new SkyHighUser
+                {
+                    UserName = Input.Username,
+                    Email = Input.Email,
+                    FullName = Input.Fullname,
+                    NormalizedEmail = Input.Email.Trim().ToUpper()
+                };
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
+                    //_logger.LogInformation("User created a new account with password.");
 
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { userId = user.Id, code = code },
-                        protocol: Request.Scheme);
+                    // var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //var callbackUrl = Url.Page(
+                    //    "/Account/ConfirmEmail",
+                    //    pageHandler: null,
+                    //    values: new { userId = user.Id, code = code },
+                    //    protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     return LocalRedirect(returnUrl);
